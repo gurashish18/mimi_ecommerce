b082e96a94ea2f622144b75d7a17ab88571bc1db
Gurashish <ggill_be19@thapar.edu>
2022-01-07 16:02:34 +0530

    Signup screen

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import OnboardScreen from './Screens/OnboardScreen'
 import LoginScreen from './Screens/LoginScreen'
+import CreateAccountScreen from './Screens/CreateAccountScreen'
 
 const App = () => {
   return (
-    <LoginScreen />
+    <CreateAccountScreen />
   )
 }
 
---
+++ Screens/CreateAccountScreen.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native'
+import Button from '../components/Button'
+import Input from '../components/Input'
+
+const CreateAccountScreen = () => {
+    const[name, setname] = useState('')
+    const[mobile, setmobile] = useState('')
+    const[email, setemail] = useState('')
+    const[password, setpassword] = useState('')
+    return (
+        <ScrollView style={{backgroundColor: '#ffffff'}} contentContainerStyle={styles.container}>
+        <Image source={require('../assets/mimi_logo.png')} style={{width: 100, height: 100, resizeMode: 'contain'}}/>
+        <Text style={{fontSize: 30, color: '#000000', fontWeight: 'bold'}}>Create Account</Text>
+        <View style={{width: '100%', paddingHorizontal: 20}}>
+            <Input value={name} setvalue={setname} placeholder="Fullname" icon="user"/>
+            <Input value={mobile} setvalue={setmobile} placeholder="Mobile Number" icon="link"/>
+            <Input value={email} setvalue={setemail} placeholder="Username" icon="user"/>
+            <Input value={password} setvalue={setpassword} placeholder="Password" icon="lock"/>
+            <Button buttontext="Sign Up"/>
+
+            <View style={{flexDirection: 'row', alignSelf: 'center', marginVertical: 20}}>
+                <Text style={{color: 'grey', fontSize: 16}}>Already have an Account? </Text>
+                <Text style={{color: '#000000', fontSize: 16, fontWeight: 'bold'}}>Log In</Text>
+            </View>
+        </View>
+        
+</ScrollView>
+    )
+}
+
+export default CreateAccountScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
