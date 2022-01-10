0bedb224a1d9b16502ee18479ee2772faf00cb05
Gurashish <ggill_be19@thapar.edu>
2022-01-10 18:09:48 +0530

    storing users to firestore

--- nav/AuthProvider.js
+++ nav/AuthProvider.js
@@ -2,6 +2,7 @@ import React, {createContext, useState} from 'react';
 import auth from '@react-native-firebase/auth';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { LoginManager, AccessToken } from 'react-native-fbsdk-next';
+import firestore from '@react-native-firebase/firestore';
 
 export const AuthContext = createContext();
 
@@ -35,7 +36,28 @@ export const AuthProvider = ({children}) => {
             const googleCredential = auth.GoogleAuthProvider.credential(idToken);
 
             // Sign-in the user with the credential
-            await auth().signInWithCredential(googleCredential);
+            await auth().signInWithCredential(googleCredential)
+            .then(() => {
+              //Once the user creation has happened successfully, we can add the currentUser into firestore
+              //with the appropriate details.
+              firestore().collection('users').doc(auth().currentUser.uid)
+              .set({
+                  name: auth().currentUser.displayName,
+                  email: auth().currentUser.email,
+                  mobile: auth().currentUser.phoneNumber,
+                  createdAt: firestore.Timestamp.fromDate(new Date()),
+                  userImg: auth().currentUser.photoURL,
+              })
+              //ensure we catch any errors at this stage to advise us if something does go wrong
+              .catch(error => {
+                  console.log('Something went wrong with added user to firestore: ', error);
+              })
+            })
+            //we need to catch the whole sign up process if it fails too.
+            .catch(error => {
+                console.log('Something went wrong with sign up: ', error);
+            });
+
           }catch(e){
             alert(e)
           }
@@ -62,16 +84,56 @@ export const AuthProvider = ({children}) => {
               const facebookCredential = auth.FacebookAuthProvider.credential(data.accessToken);
 
               // Sign-in the user with the credential
-              await auth().signInWithCredential(facebookCredential);
+              await auth().signInWithCredential(facebookCredential)
+              .then(() => {
+              //Once the user creation has happened successfully, we can add the currentUser into firestore
+              //with the appropriate details.
+              firestore().collection('users').doc(auth().currentUser.uid)
+                .set({
+                    name: auth().currentUser.displayName,
+                    email: auth().currentUser.email,
+                    mobile: auth().currentUser.phoneNumber,
+                    createdAt: firestore.Timestamp.fromDate(new Date()),
+                    userImg: auth().currentUser.photoURL,
+                })
+                //ensure we catch any errors at this stage to advise us if something does go wrong
+                .catch(error => {
+                    console.log('Something went wrong with added user to firestore: ', error);
+                })
+              })
+              //we need to catch the whole sign up process if it fails too.
+              .catch(error => {
+                  console.log('Something went wrong with sign up: ', error);
+              });
             }catch(e){
               alert(e)
             }
             setspinner(true)
         },
-        register: async (email, password) => {
+        register: async (email, password, name, mobile) => {
           setspinner(true)
           try {
             await auth().createUserWithEmailAndPassword(email, password)
+            .then(() => {
+              //Once the user creation has happened successfully, we can add the currentUser into firestore
+              //with the appropriate details.
+              firestore().collection('users').doc(auth().currentUser.uid)
+              .set({
+                  name: name,
+                  email: email,
+                  mobile: mobile,
+                  createdAt: firestore.Timestamp.fromDate(new Date()),
+                  userImg: 'https://cdn1.iconfinder.com/data/icons/business-company-1/500/image-512.png',
+              })
+              //ensure we catch any errors at this stage to advise us if something does go wrong
+              .catch(error => {
+                  console.log('Something went wrong with added user to firestore: ', error);
+              })
+            })
+            //we need to catch the whole sign up process if it fails too.
+            .catch(error => {
+                console.log('Something went wrong with sign up: ', error);
+            });
           } catch (e) {
             alert(e);
           }
