c52cb85019e21c1c14b149efe0a90edd8fb8c49b
Gurashish <ggill_be19@thapar.edu>
2022-01-11 13:26:11 +0530

    Categories creen

--- Screens/CategoriesScreen.js
+++ Screens/CategoriesScreen.js
@@ -1,11 +1,34 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Image } from 'react-native'
+import{categories} from '../API/api'
 
 const CategoriesScreen = () => {
     return (
-        <View>
-            <Text>Categories</Text>
-        </View>
+        <FlatList
+                style={{backgroundColor: '#ffffff'}}
+                ListHeaderComponent={
+                    <>
+                        <Text style={{color: '#000000', fontSize: 30, fontWeight:'bold', letterSpacing:5, alignSelf: 'center', marginVertical: 20}}>Categories</Text>
+                    </>
+                    }
+                horizontal={false} 
+                data={categories} 
+                renderItem={({item, index}) => (
+                    <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginHorizontal: 20, marginVertical: 20, borderWidth: 1, padding: 10, borderRadius: 20}}>
+                        <Image source={item.image} style={{height: 100, width: 100, resizeMode: 'contain'}}/>
+                        <View>
+                            <Text style={{color: '#000000', fontSize: 24, fontWeight: 'bold'}}>{item.name}</Text>
+                            <Text style={{color: 'grey'}}>{item.text}</Text>
+                        </View>
+                    </TouchableOpacity>
+                )}
+                keyExtractor={item => item.id}
+                ListFooterComponent={
+                    <>
+                        
+                    </>
+                }    
+            />
     )
 }
 
